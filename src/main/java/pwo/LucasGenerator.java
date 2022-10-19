a905a456b88ab72993b0a249d403f09613b32992
Tomasz Rosa <tomasz.rosa@pollub.edu.pl>
2022-10-19 19:16:22 +0200

    Wykonanie Zadania 2 - wersja 1

--- src/main/java/pwo/LucasGenerator.java
+++ src/main/java/pwo/LucasGenerator.java
@@ -1,31 +1,32 @@
 package pwo;
-
 import java.math.BigDecimal;
-
-public class FibonacciGenerator {
-
+/**
+ *
+ * @author student
+ */
+public class LucasGenerator {
     private int lastIndex = 0;
     private BigDecimal current = new BigDecimal(0);
 
-    private BigDecimal f_1 = new BigDecimal(1),
-            f_2 = new BigDecimal(0);
+    private BigDecimal l_1 = new BigDecimal(2),
+            l_2 = new BigDecimal(1);
 
     public void reset() {
         lastIndex = 0;
         current = new BigDecimal(0);
-        f_1 = new BigDecimal(1);
-        f_2 = new BigDecimal(0);
+        l_1 = new BigDecimal(2);
+        l_2 = new BigDecimal(1);
     }
 
     public BigDecimal nextTerm() {
         if (lastIndex > 1) {
-            current = f_1.add(f_2);
-            f_2 = f_1;
-            f_1 = current;
+            l_2 = l_1;
+            l_1 = current;
+            current = l_1.add(l_2);
         } else if (lastIndex == 1) {
             current = new BigDecimal(1);
         } else {
-            current = new BigDecimal(0);
+            current = new BigDecimal(2);
         }
         lastIndex++;
         return current;
--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -4,6 +4,7 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
+    public static final String FILE_NAME2 = "lucseq100.txt";
 
     public static void main(String[] args) {
 
@@ -13,6 +14,14 @@ public class Zadanie1 {
 
         if (ok) {
             System.out.println("Wynik zapisany do pliku: " + FILE_NAME);
+        } else {
+            System.out.println("Błąd");
+        }
+        
+        boolean ok2 = SequenceTools.writeToFile2(new LucasGenerator(), FROM, TO, FILE_NAME2);
+
+        if (ok2) {
+            System.out.println("Wynik zapisany do pliku: " + FILE_NAME2);
         } else {
             System.out.println("Błąd");
         }
